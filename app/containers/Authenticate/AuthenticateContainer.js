3fcefef05d8620ef0d5173361fdd7fdd1e3c4bf6
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-07-27 16:22:04 +0200

    redux binding to react 101

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,24 +1,49 @@
-import React, { Component } from 'react'
+import React, { PropTypes, Component } from 'react'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
-
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as userActionCreators from 'redux/modules/users'
 
 class AuthenticateContainer extends Component {
-  handleAuth () {
+
+  handleAuth = () => {
+    this.props.fetchingUser()
     auth().then((user) => {
-      console.log(user)
+      this.props.fetchingUserSuccess(user.uid, user, Date.now())
+      this.props.authUser(user.uid)
+
     })
+    .catch((error) => this.props.fetchingUserFailure(error))
   }
-  
   render () {
+    console.log("isFetching?", this.props.isFetching)
     return (
       <Authenticate
         onAuth={this.handleAuth}
-        isFetching={false}
-        error={''} />
+        isFetching={this.props.isFetching}
+        error={this.props.error} />
     )
   }
 }
 
+Authenticate.propTypes = {
+    fetchingUser: PropTypes.func.isRequired,
+    fetchingUserFailure: PropTypes.func.isRequired,
+    fetchingUserSuccess: PropTypes.func.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+}
 
-export default AuthenticateContainer
+function mapStateToProps(state){
+  return {
+    isFetching: state.isFetching,
+    error: state.error
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators(userActionCreators, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticateContainer)

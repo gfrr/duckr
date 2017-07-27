a9a590ad41c96aeed5c8be8b38d9efa6ae602b22
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-07-27 08:36:16 +0200

    react app 101

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class Main extends Component {
+    render() {
+      return (
+        <p>
+        Duckr
+        </p>
+      )
+    }
+
+}
+
+
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+)
